90b6d41ecb151cdf0267e5e666804bb5dab441b4
Mauricio Gamboa <magalocr@gmail.com>
2015-08-13 16:59:56 -0600

    940

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -1,18 +1,3 @@
 'use strict';
 
-angular.module('eventsGlue.directives', [])
-
-  .directive('activeBg', [function () {
-    return {
-      restrict: 'A',
-
-      link: function (scope, element) {
-        var hasClass = element.parent().hasClass('active');
-
-        if (hasClass)
-          element.prepend('<span class="arrow"></span>');
-        else
-          element.find('.arrow').remove();
-      }
-    };
-  }]);
+angular.module('eventsGlue.directives', []);
--- public/js/main.js
+++ public/js/main.js
@@ -6,19 +6,4 @@ var eventsGlue = angular.module('eventsGlue', [
 
 var directives = angular.module('eventsGlue.directives', []);;'use strict';
 
-angular.module('eventsGlue.directives', [])
-
-  .directive('activeBg', [function () {
-    return {
-      restrict: 'A',
-
-      link: function (scope, element) {
-        var hasClass = element.parent().hasClass('active');
-
-        if (hasClass)
-          element.prepend('<span class="arrow"></span>');
-        else
-          element.find('.arrow').remove();
-      }
-    };
-  }]);
+angular.module('eventsGlue.directives', []);
